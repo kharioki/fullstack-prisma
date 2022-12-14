2fed1f655101c7f454c345c5109965dde6284b61
kharioki <tony.kharioki@gmail.com>
2022-12-14 05:35:55 +0300

    pothos prisma setup

--- graphql-server/src/index.ts
+++ graphql-server/src/index.ts
@@ -1,1 +1,10 @@
-console.log('Hello World');
+import { createServer } from 'node:http'
+import { createSchema, createYoga } from 'graphql-yoga'
+
+const port = Number(process.env.API_PORT) || 4000
+
+const server = createServer()
+
+server.listen(port, () => {
+  console.log(`Server started on port ${port}`)
+})
