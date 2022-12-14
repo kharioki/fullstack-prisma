c4117d083a3e41477f3bf99ce6aad1403f1bbf1d
kharioki <tony.kharioki@gmail.com>
2022-12-14 08:45:13 +0300

    using yoga v2

--- graphql-server/src/index.ts
+++ graphql-server/src/index.ts
@@ -1,16 +1,23 @@
-import { createServer } from 'node:http'
-import { createYoga } from 'graphql-yoga'
+import { createServer } from "@graphql-yoga/node";
+// import { createYoga } from 'graphql-yoga'
 import { schema } from './schema'
 
 const port = Number(process.env.API_PORT) || 4000
 
 
-const yoga = createYoga({
+// const yoga = createYoga({
+//   schema
+// })
+
+const server = createServer({
+  port,
   schema
 })
 
-const server = createServer(yoga)
+// server.listen(port, () => {
+//   console.log(`Server started on port ${port}`)
+// })
 
-server.listen(port, () => {
+server.start().then(() => {
   console.log(`Server started on port ${port}`)
-})
+});
